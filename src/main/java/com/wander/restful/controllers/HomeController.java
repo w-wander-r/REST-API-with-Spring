ed550445c3550375f0a2c857d7ee08eefd07d04e
w-wander-r <wanderexe68@gmail.com>
2025-01-23 19:40:34 +0200

    connected db & added home controller

---
+++ src/main/java/com/wander/restful/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.wander.restful.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+    
+    @GetMapping
+    public String requestMethodName() {
+        return "index";
+    }
+    
+}
